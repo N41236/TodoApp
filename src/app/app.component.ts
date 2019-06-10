ff4eef550e7b02ec186bb603491c670f35863620
Pasagada <41236@nelnet.net>
2019-06-10 18:03:24 -0500

    Added unsunscribe for subscriptions.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TodoDataService } from './todo-data.service';
 import { TodoData } from './todo-data';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatListOption } from '@angular/material';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +11,17 @@ import { MatListOption } from '@angular/material';
   styleUrls: ['./app.component.css'],
   providers: [TodoDataService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   public listItems: string[];
+  private _getTodoSubscription: Subscription;
+  private _addTodoSubscription: Subscription;
+  private _removeTodoSubscription: Subscription;
 
   constructor(private _service: TodoDataService) { }
 
-  ngOnInit() {
-    this._service.getTodoItems().subscribe(
+  ngOnInit(): void {
+    this._getTodoSubscription = this._service.getTodoItems().subscribe(
       (data: TodoData) => {
         this.listItems = data.todoItems;
       },
@@ -27,20 +31,25 @@ export class AppComponent implements OnInit {
     );
   }
 
-  handleRemove(options: MatListOption[]) {
+  public handleRemove(options: MatListOption[]): void {
     const selected: number[] = options.map(option => option.value);
-    this._service.removeTodoItem(selected).subscribe(
+    this._removeTodoSubscription = this._service.removeTodoItem(selected).subscribe(
       data => this.listItems = data.todoItems,
       err => console.log(err)
     );
   }
 
-  handleAdd(inputVal: string) {
-    this._service.addTodoItem(inputVal).subscribe(
+  public handleAdd(inputVal: string): void {
+    this._addTodoSubscription = this._service.addTodoItem(inputVal).subscribe(
       data => this.listItems = data.todoItems,
       err => console.log(err)
     );
   }
 
+  ngOnDestroy(): void {
+    this._addTodoSubscription.unsubscribe();
+    this._removeTodoSubscription.unsubscribe();
+    this._getTodoSubscription.unsubscribe();
+  }
 }
 
